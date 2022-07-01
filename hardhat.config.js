dbae3adadf28b59ec5d0e20fa07025f6a21ed6d4
a.cobo.2020 <a.cobo.2020@alumnos.urjc.es>
2022-07-01 17:04:47 +0200

    v1.0.2
    Signed-off-by: a.cobo.2020 <a.cobo.2020@alumnos.urjc.es>

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,7 +15,7 @@ const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const KOVAN_RPC_URL =
     process.env.KOVAN_RPC_URL || "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY =
-    process.env.PRIVATE_KEY || "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+    process.env.PRIVATE_KEY || ""
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 module.exports = {
